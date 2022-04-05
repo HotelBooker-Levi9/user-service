1097a71d427f3a8b8dbac5d60c0a6e6210454a4f
GGmaz <kajtes.nikola@gmail.com>
2022-04-05 18:27:49 +0200

    feat: create email dto for client (#2)

--- src/main/java/com/example/userservice/service/ClientService.java
+++ src/main/java/com/example/userservice/service/ClientService.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.example.userservice.model.Client;
+import org.springframework.http.ResponseEntity;
 
 public interface ClientService {
 	
 	Optional<Client> getById(Long id);
     List<Client> getAll();
+
+    ResponseEntity<?> getClientEmailAndName(Long cartId);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO ROLE(id, name) values (2, "ROLE_ADMIN");
 
 
 INSERT INTO CLIENT(id, email, name, password, surname, username, role_id, is_deleted, cart_id, is_blocked, salt) values 
-(1, "user1@gmail.com", "Cvetko", "123", "Cvetkovic", "svetko1", 1, false, null, false, null);
+(1, "user1@gmail.com", "Cvetko", "123", "Cvetkovic", "svetko1", 1, false, 1, false, null);
