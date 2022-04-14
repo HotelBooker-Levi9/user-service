2687aeb9dd6658a92f65e49e2bb2ddb1209c59fa
ABjelobaba <bjelobaba.ra136.2018@uns.ac.rs>
2022-04-14 09:20:14 +0200

    chore: remove imports

--- src/main/java/com/example/userservice/security/WebSecurityConfig.java
+++ src/main/java/com/example/userservice/security/WebSecurityConfig.java
@@ -6,15 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
